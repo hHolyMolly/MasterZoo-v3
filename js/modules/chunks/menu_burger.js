ac6770ac22ba2da7b13df9e0c596143810fe8022
HolyMolly <developer.code5417@gmail.com>
2024-05-22 15:12:57 +0300

    Updates

--- js/modules/chunks/menu_burger.js
+++ js/modules/chunks/menu_burger.js
@@ -1,13 +1,15 @@
 import { bodyLock, bodyUnLock } from '../functions/body_lock.js';
 
 (() => {
-  const header = document.querySelector('.header');
+  const header = document.querySelector('.header__wrapper');
+
   if (header) {
     const burgerIcon = header.querySelector('[data-burger-icon]');
     const menuBody = header.querySelector('[data-header-menu]');
 
     if (burgerIcon && menuBody) {
       burgerIcon.addEventListener('click', () => {
+        header.classList.toggle('_active');
         burgerIcon.classList.toggle('_active');
         menuBody.classList.toggle('_active');
 
